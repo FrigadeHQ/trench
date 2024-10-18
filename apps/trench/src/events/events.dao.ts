434d85d6dafa7c5137e1c24d0f9116aa5a64f8ae
christianmat <christian_mathiesen@alumni.brown.edu>
2024-10-17 17:17:50 -0700

    fix: deserialization issue when creating events

--- apps/trench/src/events/events.dao.ts
+++ apps/trench/src/events/events.dao.ts
@@ -133,9 +133,21 @@ export class EventsDao {
       groupId: row.group_id ? row.group_id : undefined,
       anonymousId: row.anonymous_id ? row.anonymous_id : undefined,
       instanceId: row.instance_id ? row.instance_id : undefined,
-      properties: row.properties ? JSON.parse(row.properties) : undefined,
-      traits: row.traits ? JSON.parse(row.traits) : undefined,
-      context: row.context ? JSON.parse(row.context) : undefined,
+      properties: row.properties
+        ? typeof row.properties === 'string'
+          ? JSON.parse(row.properties)
+          : row.properties
+        : undefined,
+      traits: row.traits
+        ? typeof row.traits === 'string'
+          ? JSON.parse(row.traits)
+          : row.traits
+        : undefined,
+      context: row.context
+        ? typeof row.context === 'string'
+          ? JSON.parse(row.context)
+          : row.context
+        : undefined,
       timestamp: new Date(row.timestamp),
       parsedAt: new Date(row.parsed_at),
     }
