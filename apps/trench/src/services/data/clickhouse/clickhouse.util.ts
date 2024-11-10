e44aa5d5710fe7c7eb6f3b72ccc4e287b243b57a
Max <max.nelkner@web.de>
2024-11-10 18:04:27 +0100

    fix: filter dates for clickhouse query (#21)

--- apps/trench/src/services/data/clickhouse/clickhouse.util.ts
+++ apps/trench/src/services/data/clickhouse/clickhouse.util.ts
@@ -4,3 +4,15 @@ export function escapeString(str: string) {
     .replace(/'/g, "\\'") // Escape single quotes
     .replace(/"/g, '\\"') // Escape double quotes
 }
+
+/**
+ * Formats a date to be used in a ClickHouse query.
+ *
+ * The date will be formatted as ISO 8601, without the timezone "Z" at the end.
+ * The date will also be escaped to be safe to use in a ClickHouse query.
+ */
+export function formatToClickhouseDate(date: Date): string {
+  const isoString = date.toISOString();
+  const clickhouseDate = isoString.replace('Z', '');
+  return escapeString(clickhouseDate);
+}
