d3c530e30d07107c1bbfffba122335bb843d28fc
tuminzee <58180803+tuminzee@users.noreply.github.com>
2024-12-12 05:09:33 +0530

    Transition to nestjs/logger (#38)
    * replace console.logs with nests/logger
    
    * refactor: inline CPU count in cluster mode logging

--- apps/trench/src/appCluster.service.ts
+++ apps/trench/src/appCluster.service.ts
@@ -1,6 +1,6 @@
 import * as _cluster from 'cluster'
 import * as os from 'os'
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 
 const cluster = _cluster as unknown as _cluster.Cluster // typings fix
 
@@ -8,19 +8,20 @@ const numCPUs = os.cpus().length
 
 @Injectable()
 export class AppClusterService {
+  private static readonly logger = new Logger(AppClusterService.name)
   static clusterize(callback: Function): void {
     if (cluster.isPrimary) {
-      console.log(`Primary server started on ${process.pid} (using ${numCPUs} processes).`)
+      this.logger.log(`Primary server started on ${process.pid} (using ${numCPUs} processes).`)
       for (let i = 0; i < numCPUs; i++) {
         cluster.fork()
       }
       cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died. Restarting`)
+        this.logger.log(`Worker ${worker.process.pid} died. Restarting`)
         cluster.fork()
       })
     } else {
       const nodeNumber = cluster.worker.id
-      console.log(`Cluster server started on ${process.pid}`)
+      this.logger.log(`Cluster server started on ${process.pid}`)
       callback(nodeNumber)
     }
   }
